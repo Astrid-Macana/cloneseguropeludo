85ec325e21a3228e34f72ef68f63138402f1f7a8
Astrid-Macana <lorenamakna21@gmail.com>
2023-01-26 18:44:11 -0500

    se crea la tabla para el componente plans

--- src/component/plans/index.js
+++ src/component/plans/index.js
@@ -5,13 +5,21 @@ import { DiferentPlanes } from "./style";
 const Planes = () => {
   return (
     <DiferentPlanes>
-      <section className='contenedor'>
-        <article className='planes'>
+      <section className="contenedor">
+        <article className="planes">
           <h2>Nuestros Planes</h2>
           <p>Planeados pensando en la tranquilidad de tu peludito</p>
-         <div>
-          
-         </div>
+          <div className="table">
+            <div className="table-header">Básico</div>
+            <div className="table-header">Estándar</div>
+            <div className="table-header">Premium</div>
+            <div className="table-item">20000</div>
+            <div className="table-item">28000</div>
+            <div className="table-item">36000</div>
+            <div className="table-item">Mensual</div>
+            <div className="table-item">Mensual</div>
+            <div className="table-item">Mensual</div>
+          </div>
           <h2>Beneficios de seguro </h2>
           <p>
             Diseñamos nuestros planes para que sean simples y sencillos, de modo
--- src/component/plans/style.js
+++ src/component/plans/style.js
@@ -1,12 +1,9 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 import imageBackground from "../../imagenes/backgroud.png";
-
-
 
 export const DiferentPlanes = styled.section`
   background-image: url(${imageBackground});
   height: 60rem;
-
   background-repeat: repeat;
   background-size: 150px;
 
@@ -20,7 +17,7 @@ export const DiferentPlanes = styled.section`
       width: 100%;
       max-width: 752px;
       margin: auto;
-
+    }git
       h2 {
         color: white;
         font-size: 30;
@@ -33,9 +30,36 @@ export const DiferentPlanes = styled.section`
         color: white;
         text-align: center;
       }
+      .table{
+    width: 90%;
+    background-color:white;
+    margin: 50px auto;
+    display:grid;
+    grid-template-columns: repeat(3,1fr) ;
+    grid-auto-rows:50px;
+    grid-column-start: 1;
+    grid-column-end:4;
+    
+}
+.table-header, .table-item{
+    display: flex;
+    justify-content:center;
+    align-items: center;
+     line-height:30px ;
+
+}
+.table-header{
+    font-weight: bold;
+    font-size: 1.5em;
+    background-color:var( --blue3);
+    color:white
+
+
+    }
+    .table-item{
+        font-size:24px;
+        padding:10px;
+        color:var(--blue3) ;
     }
   }
 `;
-
-
-
