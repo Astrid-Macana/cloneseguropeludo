735c6579ff0de5a8c8785516c9ba73e867d54555
Astrid-Macana <lorenamakna21@gmail.com>
2023-01-27 12:44:05 -0500

    se restructuroo tabla del componente de plans

--- src/component/plans/index.js
+++ src/component/plans/index.js
@@ -6,25 +6,36 @@ const Planes = () => {
   return (
     <DiferentPlanes>
       <section className="contenedor">
-        <article className="planes">
-          <h2>Nuestros Planes</h2>
-          <p>Planeados pensando en la tranquilidad de tu peludito</p>
-          <div className="table">
-            <div className="table-header">Básico</div>
-            <div className="table-header">Estándar</div>
-            <div className="table-header">Premium</div>
-            <div className="table-item">20000</div>
-            <div className="table-item">28000</div>
-            <div className="table-item">36000</div>
-            <div className="table-item">Mensual</div>
-            <div className="table-item">Mensual</div>
-            <div className="table-item">Mensual</div>
+        <article className="nuestros-planes">
+          <h1>Nuestros Planes</h1>
+          <p id="parf-planes">
+            Planeados pensando en la tranquilidad de tu peludito
+          </p>
+          <div className="container-table">
+            <div className="table-plane-columnBasico">
+              <p className="p-basic">Básico</p>
+              <p className="price-basic">$20.000</p>
+              <p className="mensual-b">Mensual</p>
+            </div>
+            <div className="table-plane-column-Estandar" id="estandar">
+              <div className="column-yellow">
+                <p>Popular</p>
+              </div>
+              <p className='p-estandar'>Estándar</p>
+              <p classNme="price-estandar">$28.000</p>
+              <p className="mensual-e">Mensual</p>
+            </div>
+            <div className="table-plane-columnPremium">
+              <p className="p-premium">Premium</p>
+              <p className="price-premium">36.000</p>
+              <p className="mensual-p">Mensual</p>
+            </div>
           </div>
-          <h2>Beneficios de seguro </h2>
-          <p>
+          <h1>Beneficios de Seguro Peludo</h1>
+          <p className="parf-planes">
             Diseñamos nuestros planes para que sean simples y sencillos, de modo
             que puedas pasar menos tiempo eligiendo y más tiempo con tu
-            peludito.{" "}
+            peludito.
           </p>
         </article>
       </section>
--- src/component/plans/style.js
+++ src/component/plans/style.js
@@ -12,54 +12,73 @@ export const DiferentPlanes = styled.section`
     flex-direction: column;
     padding: 0 30px;
 
-    .planes {
+    .nuestros-planes {
       display: block;
       width: 100%;
       max-width: 752px;
       margin: auto;
-    }git
-      h2 {
-        color: white;
-        font-size: 30;
-        padding-top: 50px;
-        padding-bottom: 30px;
-        font-family: var(--family-titlle);
-        text-align: center;
-      }
-      p {
-        color: white;
-        text-align: center;
-      }
-      .table{
-    width: 90%;
-    background-color:white;
-    margin: 50px auto;
-    display:grid;
-    grid-template-columns: repeat(3,1fr) ;
-    grid-auto-rows:50px;
-    grid-column-start: 1;
-    grid-column-end:4;
-    
-}
-.table-header, .table-item{
-    display: flex;
-    justify-content:center;
-    align-items: center;
-     line-height:30px ;
-
-}
-.table-header{
-    font-weight: bold;
-    font-size: 1.5em;
-    background-color:var( --blue3);
-    color:white
-
-
     }
-    .table-item{
-        font-size:24px;
-        padding:10px;
-        color:var(--blue3) ;
+    h1 {
+      color: white;
+      font-size: 30;
+      padding-top: 50px;
+      padding-bottom: 30px;
+      font-family: var(--family-titlle);
+      text-align: center;
+    }
+    p {
+      color: white;
+      text-align: center;
+    }
+    .container-table {
+      width: 90%;
+      background-color: white;
+      margin: 50px auto;
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      grid-auto-rows: 50px;
+      grid-column-start: 1;
+      grid-column-end: 4;
+      border-radius: 8px;
+    }
+    .table-plane-columnBasico,
+    .table-plane-column-Estandar,
+    .table-plane-columnPremium {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      line-height: 30px;
+    }
+    .table-header {
+      font-weight: bold;
+      font-size: 1.5em;
+      background-color: var(--blue3);
+      color: white;
+    }
+    .p-basic,
+    .p-premium {
+      font-size: 24px;
+      padding: 10px;
+      color: var(--blue3);
+      background-color: #efefef;
+    }
+    .p-estandar {
+      font-weight: lighter;
+      font-size: 1.5em;
+      background-color: var(--yellow3);
+      color: var(--fondo);
+    }
+    .p-basic {
+      border-radius: 8px 0 0;
+    }
+    .p-premium {
+      border-radius: 0 8px 0 0;
+    }
+
+    .table-item {
+      font-size: 24px;
+      padding: 10px;
+      color: var(--blue3);
     }
   }
 `;
